a84c6fa7bb1be450640ca5d52890e0a899376942
Arsalan Khattak <me@khattak.dev>
2022-03-09 20:05:27 +0500

    Add queues

---
+++ queue/index.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    const temp = this.first;
+    if (this.first == this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
