8a610fd6a05d8c394894ece35475360a1747fe4d
Arsalan Khattak <me@khattak.dev>
2022-03-23 20:19:18 +0500

    add dfs traversals

--- tree/index.js
+++ tree/index.js
@@ -60,4 +60,43 @@ class BinarySearchTree {
       }
     }
   }
+
+  DFSPreOrder() {
+    var data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder() {
+    var data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  DFSInOrder() {
+    var data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
