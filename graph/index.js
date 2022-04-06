5d9b45743965d3aebc3e368f89ffad4b0a30a8a2
Arsalan Khattak <me@khattak.dev>
2022-04-06 21:15:15 +0500

    add custom filter function

--- graph/index.js
+++ graph/index.js
@@ -13,7 +13,22 @@ class Graph {
   }
 
   removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
-    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+    // this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v1] = this.filterVertices(v1, v2);
+
+    // this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+    this.adjacencyList[v2] = this.filterVertices(v2, v1);
+  }
+
+  //
+
+  filterVertices(v1, v2) {
+    const newArray = [];
+    for (let i = 0; i < this.adjacencyList[v1].length; i++) {
+      if (this.adjacencyList[v1][i] !== v2) {
+        newArray.push(this.adjacencyList[v1][i]);
+      }
+    }
+    return newArray;
   }
 }
